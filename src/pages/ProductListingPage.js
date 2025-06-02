9f495a257ba4916743daff628351b152d61e09dd
Bruno Peres <brunowebdeveloper33@gmail.com>
2025-06-02 19:40:57 +0300

    Pages added

---
+++ src/pages/ProductListingPage.js
@@ -0,0 +1,42 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart } from '../redux/cartSlice';
+import { plantData } from '../plantData';
+import Header from '../components/Header';
+
+export default function ProductListingPage() {
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.items);
+
+  const grouped = {};
+  plantData.forEach(p => {
+    if (!grouped[p.category]) grouped[p.category] = [];
+    grouped[p.category].push(p);
+  });
+
+  return (
+    <div>
+      <Header />
+      {Object.keys(grouped).map(cat => (
+        <div key={cat}>
+          <h2>{cat}</h2>
+          <div className="card-group">
+            {grouped[cat].map(p => (
+              <div key={p.id} className="card">
+                <img src={p.image} alt={p.name} />
+                <h3>{p.name}</h3>
+                <p>{p.description}</p>
+                <p>${p.price}</p>
+                <button
+                  onClick={() => dispatch(addToCart(p))}
+                  disabled={cart[p.id]}
+                >
+                  {cart[p.id] ? 'Added to Cart' : 'Add to Cart'}
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ src/pages/products.js
@@ -0,0 +1,6 @@
+export const plantData = [
+  { id: '1', name: 'Peace Lily', price: 20, category: 'Air Purifying', description: 'Cleans indoor air', image: '/img/lily.jpg' },
+  { id: '2', name: 'Lavender', price: 15, category: 'Aromatic', description: 'Smells great', image: '/img/lavender.jpg' },
+  { id: '3', name: 'Snake Plant', price: 18, category: 'Low Light', description: 'Thrives in shade', image: '/img/snake.jpg' },
+  // Add 3+ more with mixed categories
+];
